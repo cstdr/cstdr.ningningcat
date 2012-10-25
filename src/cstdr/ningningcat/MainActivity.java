44fc47c26bc0cf44ede333456e5d2431ada0c38d
丁冉 <cstdingran@gmail.com>
2012-10-26 00:23:43 +0800

    format

--- src/cstdr/ningningcat/MainActivity.java
+++ src/cstdr/ningningcat/MainActivity.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
-import android.R.color;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
@@ -13,7 +12,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
