27c551166a630d7bf08d91ccc160a20fb52b46c0
cstdr <cstdingran@gmail.com>
2013-02-21 23:24:53 +0800

    orientation portrait and dialog.

--- src/cstdr/ningningcat/util/DialogUtil.java
+++ src/cstdr/ningningcat/util/DialogUtil.java
@@ -1,6 +1,7 @@
 package cstdr.ningningcat.util;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
@@ -23,6 +24,18 @@ import cstdr.ningningcat.R;
  */
 public class DialogUtil {
 
+    private static Dialog jsAlertDialog; // 页面JS警告框
+
+    private static Dialog jsConfirmDialog; // 页面JS确认框
+
+    private static Dialog alertDialog; // 警告框
+
+    private static Dialog noConnectDialog; // 无网状态框
+
+    private static Dialog favoriteDialog; // 长按收藏夹Item弹出框
+
+    private static Dialog renameDialog; // 显示重命名收藏页面的弹出框
+
     /** 手机设置页面ACTION **/
     private static final String ACTION_SETTINGS="android.settings.SETTINGS";
 
@@ -33,14 +46,19 @@ public class DialogUtil {
      * @param result
      */
     public static void showJsAlertDialog(Context context, String message, final JsResult result) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(R.string.title_alert).setMessage(message).setPositiveButton(android.R.string.ok, new OnClickListener() {
+        if(jsAlertDialog != null && jsAlertDialog.isShowing()) {
+            return;
+        }
+        jsAlertDialog=
+            new AlertDialog.Builder(context).setTitle(R.string.title_alert).setMessage(message)
+                .setPositiveButton(android.R.string.ok, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                result.confirm();
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                }).create();
+        jsAlertDialog.show();
 
     }
 
@@ -51,20 +69,25 @@ public class DialogUtil {
      * @param result
      */
     public static void showJsConfirmDialog(Context context, String message, final JsResult result) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(R.string.title_confirm).setMessage(message).setPositiveButton(android.R.string.ok, new OnClickListener() {
+        if(jsConfirmDialog != null && jsConfirmDialog.isShowing()) {
+            return;
+        }
+        jsConfirmDialog=
+            new AlertDialog.Builder(context).setTitle(R.string.title_confirm).setMessage(message)
+                .setPositiveButton(android.R.string.ok, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                result.confirm();
-            }
-        }).setNegativeButton(android.R.string.cancel, new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                }).setNegativeButton(android.R.string.cancel, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                result.cancel();
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.cancel();
+                    }
+                }).create();
+        jsConfirmDialog.show();
 
     }
 
@@ -74,13 +97,18 @@ public class DialogUtil {
      * @param message
      */
     public static void showAlertDialog(Context context, String message) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(R.string.title_alert).setMessage(message).setPositiveButton(android.R.string.ok, new OnClickListener() {
+        if(alertDialog != null && alertDialog.isShowing()) {
+            return;
+        }
+        alertDialog=
+            new AlertDialog.Builder(context).setTitle(R.string.title_alert).setMessage(message)
+                .setPositiveButton(android.R.string.ok, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).create();
+        alertDialog.show();
     }
 
     /**
@@ -88,23 +116,27 @@ public class DialogUtil {
      * @param context
      */
     public static void showNoConnectDialog(final Context context) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(R.string.title_no_connect).setMessage(R.string.msg_no_connect)
-            .setPositiveButton(R.string.btn_cancel, new OnClickListener() {
+        if(noConnectDialog != null && noConnectDialog.isShowing()) {
+            return;
+        }
+        noConnectDialog=
+            new AlertDialog.Builder(context).setTitle(R.string.title_no_connect).setMessage(R.string.msg_no_connect)
+                .setPositiveButton(R.string.btn_cancel, new OnClickListener() {
 
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
-                }
-            }).setNegativeButton(R.string.btn_ok, new OnClickListener() {
+                    }
+                }).setNegativeButton(R.string.btn_ok, new OnClickListener() {
 
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    MainActivity.getInstance().setNetworkMode(true); // 避免了设置完返回界面时二次提示
-                    Intent intent=new Intent(ACTION_SETTINGS);
-                    context.startActivity(intent);
-                }
-            }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.getInstance().setNetworkMode(true); // 避免了设置完返回界面时二次提示
+                        Intent intent=new Intent(ACTION_SETTINGS);
+                        context.startActivity(intent);
+                    }
+                }).create();
+        noConnectDialog.show();
     }
 
     /**
@@ -115,14 +147,19 @@ public class DialogUtil {
      * @param listener
      */
     public static void showFavoriteDialog(Context context, String title, final int position, final DialogItemClickListener listener) {
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(title).setItems(new String[]{"添加快捷方式到桌面", "设为首页", "重命名", "分享", "删除"}, new OnClickListener() {
+        if(favoriteDialog != null && favoriteDialog.isShowing()) {
+            return;
+        }
+        favoriteDialog=
+            new AlertDialog.Builder(context).setTitle(title)
+                .setItems(new String[]{"添加快捷方式到桌面", "设为首页", "重命名", "分享", "删除"}, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                listener.onClick(position, which);
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.onClick(position, which);
+                    }
+                }).create();
+        favoriteDialog.show();
     }
 
     /**
@@ -133,6 +170,9 @@ public class DialogUtil {
      * @param listener
      */
     public static void showRenameDialog(Context context, String title, final String url, final DialogRenameListener listener) {
+        if(renameDialog != null && renameDialog.isShowing()) {
+            return;
+        }
         final EditText editText=new EditText(context);
         LayoutParams params=new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         editText.setLayoutParams(params);
@@ -146,26 +186,26 @@ public class DialogUtil {
                 return false;
             }
         });
+        renameDialog=
+            new AlertDialog.Builder(context).setTitle(title).setView(editText)
+                .setPositiveButton(R.string.btn_cancel, new OnClickListener() {
 
-        AlertDialog.Builder builder=new AlertDialog.Builder(context);
-        builder.setTitle(title).setView(editText).setPositiveButton(R.string.btn_cancel, new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).setNegativeButton(R.string.btn_ok, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        }).setNegativeButton(R.string.btn_ok, new OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String titleStr=editText.getText().toString();
-                if(TextUtils.isEmpty(titleStr)) {
-                    titleStr="宁宁猫的未知网页";
-                } else if(titleStr.length() > 20) { // 数据库中定义title长度为20
-                    titleStr=titleStr.substring(0, 20);
-                }
-                listener.onClick(titleStr, url);
-
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String titleStr=editText.getText().toString();
+                        if(TextUtils.isEmpty(titleStr)) {
+                            titleStr="宁宁猫的未知网页";
+                        } else if(titleStr.length() > 20) { // 数据库中定义title长度为20
+                            titleStr=titleStr.substring(0, 20);
+                        }
+                        listener.onClick(titleStr, url);
+                    }
+                }).create();
+        renameDialog.show();
     }
 }
